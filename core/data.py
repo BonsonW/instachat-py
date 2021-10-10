05648eac936200276b91b04f5883835c03d964b1
TheRealBonson <bonson.ym@gmail.com>
2021-10-10 18:47:50 +1100

    added basic functionality for data

--- core/data.py
+++ core/data.py
@@ -3,10 +3,22 @@ from core import auth, user
 
 users = []
 
+#region methods
+
+def get_user(name):
+    for user in users:
+        if user.name == name:
+            return user
+    return None
+
+def add_user(name):
+    users.append(user.User(name))
+
+#endregion
+
 # add all existing users to database
 with open(auth.cred_path, "r") as f:
     creds = f.readlines()
 for cred in creds:
     name = cred.split()[0]
-    users.append(user.User(name))
-
+    add_user(name)
--- tests/data_test.py
+++ tests/data_test.py
@@ -1,6 +1,27 @@
+# external
+import pytest
+
 # internal
 from core import data, auth
 
-def test_user_list():
+
+#region fixtures
+
+@pytest.fixture
+def missing_user_cred():
+    return ("bar", "foo")
+
+#endregion
+
+
+#region tests
+
+def test_user_list_initialized():
     for user in data.users:
         assert auth.user_exists(user.name)
+
+def test_add_user_success(missing_user_cred):
+    data.add_user(missing_user_cred[0])
+    assert data.get_user(missing_user_cred[0]) is not None
+
+#endregion
