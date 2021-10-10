8c12958981129c80521349adf68dfb04bcb68ba0
TheRealBonson <bonson.ym@gmail.com>
2021-10-10 12:51:16 +1100

    client cleanup

--- core/client.py
+++ core/client.py
@@ -1,15 +1,7 @@
-"""
-    Python 3
-    Usage: python3 TCPClient3.py localhost 12000
-    coding: utf-8
-    
-    Author: Wei Song (Tutor for COMP3331/9331)
-"""
- 
-
 from socket import *
 import sys
 
+#region establish connection
 
 # Server would be running on the same host as Client
 if len(sys.argv) != 3:
@@ -25,24 +17,21 @@ clientSocket = socket(AF_INET, SOCK_STREAM)
 # build connection with the server and send message to it
 clientSocket.connect(serverAddress)
 
+#endregion
+
+#region client loop
+
 while True:
+    # send method
     message = input("===== Please type any messsage you want to send to server: =====\n")
     clientSocket.sendall(message.encode())
 
-    # receive response from the server
-    # 1024 is a suggested packet size, you can specify it as 2048 or others
+    # recieve response
     data = clientSocket.recv(1024)
-    receivedMessage = data.decode()
+    status, response = data.decode()
 
-    # parse the message received from server and take corresponding actions
-    if receivedMessage == "":
-        print("[recv] Message from server is empty!")
-    elif receivedMessage == "user credentials request":
-        print("[recv] You need to provide username and password to login")
-    elif receivedMessage == "download filename":
-        print("[recv] You need to provide the file name you want to download")
-    else:
-        print("[recv] Message makes no sense")
+    # print response given by server
+    print(response)
         
     ans = input('\nDo you want to continue(y/n) :')
     if ans == 'y':
@@ -50,5 +39,11 @@ while True:
     else:
         break
 
+#end region
+
 # close the socket
 clientSocket.close()
+
+#region client methods
+
+#endregion
